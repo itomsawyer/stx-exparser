f988cb4a5ac6ba7098d8385f905bf3c8f277a2c9
Cyril Giraud <cgiraud@free.fr>
2015-01-26 23:52:49 +0100

    Update for compatibility with boost library 1.54.

--- libstx-exparser/ExpressionParser.cc
+++ libstx-exparser/ExpressionParser.cc
@@ -20,22 +20,22 @@
  */
 
 /** \file ExpressionParser.cc
- * Implementation of the parser using a boost::spirit grammar and a different
+ * Implementation of the parser using a boost::spirit::classic grammar and a different
  * specializations of ParseNode.
  */
 
 #include "ExpressionParser.h"
 #include <string.h>
 
-#include <boost/spirit/core.hpp>
+#include <boost/spirit/home/classic/core.hpp>
 
-#include <boost/spirit/tree/ast.hpp>
-#include <boost/spirit/tree/tree_to_xml.hpp>
+#include <boost/spirit/home/classic/tree/ast.hpp>
+#include <boost/spirit/home/classic/tree/tree_to_xml.hpp>
 
-#include <boost/spirit/utility/lists.hpp>
-#include <boost/spirit/utility/distinct.hpp>
-#include <boost/spirit/utility/escape_char.hpp>
-#include <boost/spirit/utility/grammar_def.hpp> 
+#include <boost/spirit/home/classic/utility/lists.hpp>
+#include <boost/spirit/home/classic/utility/distinct.hpp>
+#include <boost/spirit/home/classic/utility/escape_char.hpp>
+#include <boost/spirit/home/classic/utility/grammar_def.hpp> 
 
 #include <iostream>
 #include <sstream>
@@ -49,7 +49,7 @@ namespace stx {
 /// implementation classes.
 namespace Grammar {
 
-using namespace boost::spirit;
+using namespace boost::spirit::classic;
 
 /// This enum specifies ids for the parse tree nodes created for each rule.
 enum parser_ids
@@ -87,10 +87,10 @@ enum parser_ids
 /// Keyword parser used for matching words with () and spaces as separators.
 distinct_parser<> keyword_p("a-zA-Z0-9_");
 
-/// The boost::spirit expression parser grammar
+/// The boost::spirit::classic expression parser grammar
 struct ExpressionGrammar : public grammar<ExpressionGrammar>
 {
-    /// The boost::spirit expression parser grammar definition (for a specific
+    /// The boost::spirit::classic expression parser grammar definition (for a specific
     /// scanner) with two entry points.
     template <typename ScannerT>
     struct definition : public grammar_def<rule<ScannerT, parser_context<>, parser_tag<expr_id> >,
@@ -1274,7 +1274,7 @@ ParseTreeList build_exprlist(TreeIterT const &i)
     return ptlist;
 }
 
-/// Uses boost::spirit function to convert the parse tree into a XML document.
+/// Uses boost::spirit::classic function to convert the parse tree into a XML document.
 static inline void tree_dump_xml(std::ostream &os, const std::string &input, const tree_parse_info<InputIterT> &info)
 {
     // map used by the xml dumper to label the nodes
@@ -1323,9 +1323,9 @@ const ParseTree parseExpression(const std::string &input)
 #endif
 
     Grammar::tree_parse_info<Grammar::InputIterT> info =
-	boost::spirit::ast_parse(input.begin(), input.end(),
+	boost::spirit::classic::ast_parse(input.begin(), input.end(),
 				 g.use_parser<0>(),	// use first entry point: expr
-				 boost::spirit::space_p);
+				 boost::spirit::classic::space_p);
 
     if (!info.full)
     {
@@ -1351,9 +1351,9 @@ std::string parseExpressionXML(const std::string &input)
 #endif
 
     Grammar::tree_parse_info<Grammar::InputIterT> info =
-	boost::spirit::ast_parse(input.begin(), input.end(),
+	boost::spirit::classic::ast_parse(input.begin(), input.end(),
 				 g.use_parser<0>(),	// use first entry point: expr
-				 boost::spirit::space_p);
+				 boost::spirit::classic::space_p);
 
     if (!info.full)
     {
@@ -1381,9 +1381,9 @@ ParseTreeList parseExpressionList(const std::string &input)
 #endif
 
     Grammar::tree_parse_info<Grammar::InputIterT> info =
-	boost::spirit::ast_parse(input.begin(), input.end(),
+	boost::spirit::classic::ast_parse(input.begin(), input.end(),
 				 g.use_parser<1>(),	// use second entry point: exprlist
-				 boost::spirit::space_p);
+				 boost::spirit::classic::space_p);
 
     if (!info.full)
     {
